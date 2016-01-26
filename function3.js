d170d439f56cadf3af22f4556ded8eec9990c8c8
Ilana <bachwasbaroque@gmail.com>
2016-01-25 17:36:16 -0700

    first try

---
+++ function3.js
@@ -0,0 +1,25 @@
+
+//1. GetName Function
+// var info = {
+// 	name: 'ilana',
+// 	age: 28
+// }
+
+// function getName(){
+// 	return info.name;
+// }
+
+// console.log(info.name)
+
+/*2. Write a function called 'totalLetters' which takes an 
+array of strings and returns the total number of letters in all strings.*/
+
+function totalLetters() {
+for(i = 0; i <= words.length; i++)
+	var words = str.length; 
+	return str.length;  
+}
+
+var words =['potato', 'milk', 'grapes', 'tea']
+
+console.log(words)
