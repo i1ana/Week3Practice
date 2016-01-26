c7526b682a81c47d3c7ce286b466c06e0bb2bce2
Ilana <bachwasbaroque@gmail.com>
2016-01-26 12:02:08 -0700

    continuing functions

--- function3.js
+++ function3.js
@@ -14,12 +14,31 @@
 /*2. Write a function called 'totalLetters' which takes an 
 array of strings and returns the total number of letters in all strings.*/
 
-function totalLetters() {
-for(i = 0; i <= words.length; i++)
-	var words = str.length; 
-	return str.length;  
+// var words =['potato', 'milk', 'grapes', 'tea'] 
+
+// function totalLetters() {
+// for(i = 0; i <= words.length; i++)
+// 	var length = words.length; 
+// 	return length;  
+// }
+// 	console.log(length)
+
+
+/* 3. Write a function called 'keyValue' which takes two arguments and
+returns a new object with a key of the first argument and the value of the second argument. */
+
+var keyValue = function (a, b) {
+    var object = {}
+    object[a] = b
+    return object 
 }
 
-var words =['potato', 'milk', 'grapes', 'tea']
+console.log(keyValue('city', 'Denver'));
 
-console.log(words)
+/* 4. Write a function called 'negativeIndex' which takes an array and a negative number, 
+and returns the value from the array at the given negative index, as if the array was circular,
+ i.e. arr.length+num. */
+
+var negativeIndex = function() {
+
+}
