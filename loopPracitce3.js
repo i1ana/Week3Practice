cf118e482a1dbdc3ee87dbef3cf24e602cb8202c
Ilana <bachwasbaroque@gmail.com>
2016-01-27 12:22:55 -0700

    lunch time

---
+++ loopPracitce3.js
@@ -0,0 +1,28 @@
+
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+
+
+// 1. 
+for(var i=0; i<students.length; i++) {
+ console.log(students[i].age);
+}
---
+++ loopPractice3.js
@@ -0,0 +1,40 @@
+
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+
+
+// 1. 
+// for(var i=0; i<students.length; i++) {
+//  console.log(students[i].age);
+// }
+
+// 2. 
+// for(var i=0; i<students.length; i++) {
+//  console.log(students[i].name, students[i].city);
+// }
+
+// 3. 
+for(var i=0; i<students.length; i++) {
+	if(students.city = Boulder){
+ 		console.log(students[i].name + " is from " + "Boulder" in students );
+ 	}
+}
