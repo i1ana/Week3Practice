a27c7b7ba9215d45a2d11d1cca96d4efebd409e9
Ilana <bachwasbaroque@gmail.com>
2016-01-28 15:37:14 -0700

    part one

--- infiniteAgenda.js
+++ infiniteAgenda.js
@@ -3,13 +3,25 @@ angular.module('agenda', [])
 	angular.module('agenda')
 		.controller('agendaController', ['$scope', function($scope){
 
+			var agenda=$scope;
 
+			agenda.today = Date();
+			agenda.dateObj = new Date();
+			console.log(agenda);
+				
+			for (key in agenda.dateObj){
+			console.log(key);
+			}
 
-
-
-
+		console.log("date ",agenda.today);
+		console.log(typeof agenda.today);
 
 
 			
-		}])
+
+
+
+
+
+}])
 
